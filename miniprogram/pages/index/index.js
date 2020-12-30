18b53dcb9a826e8a8f25b2e399b75e234d45581c
lixuetao <lixuetaoleo@163.com>
2020-12-30 23:43:00 +0800

    initial commit

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,25 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+
+  },
+
+  onLoad: function () {},
+
+
+  onGetUserInfo: function (e) {
+
+  },
+
+  onGetOpenid: function () {
+
+  },
+
+  // 上传图片
+  doUpload: function () {
+
+  },
+
+})
