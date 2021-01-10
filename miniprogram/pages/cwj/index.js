acac4ea138570cfc2cb374431918f2226e9f6d8d
lixuetao <lixuetaoleo@163.com>
2021-01-10 23:00:59 +0800

    修改页面

--- miniprogram/pages/cwj/index.js
+++ miniprogram/pages/cwj/index.js
@@ -1,66 +1,111 @@
-// pages/cwj/index.js
+//index.js
+const app = getApp();
+const dataAtSchool = {
+  sfzx: 1,
+  tw: 1,
+  area: "陕西省 西安市 雁塔区",
+  city: "西安市",
+  province: "陕西省",
+  address: "陕西省西安市雁塔区电子城街道西安电子科技大学(北校区)",
+  geo_api_info: {
+    "type": "complete",
+    "info": "SUCCESS",
+    "status": 1,
+    "position": {
+      "Q": 34.2325,
+      "R": 108.91436399999999,
+      "lng": 108.9144,
+      "lat": 34.2325
+    },
+    "message": "Get ipLocation success.Get address success.",
+    "location_type": "ip",
+    "accuracy": null,
+    "isConverted": true,
+    "addressComponent": {
+      "citycode": "029",
+      "adcode": "610113",
+      "businessAreas": [{
+        "name": "电子城",
+        "id": "610113",
+        "location": {
+          "Q": 34.205716,
+          "R": 108.91483199999999,
+          "lng": 108.914832,
+          "lat": 34.205716
+        }
+      }],
+      "neighborhoodType": "",
+      "neighborhood": "",
+      "building": "",
+      "buildingType": "",
+      "street": "太白南路",
+      "streetNumber": "2号",
+      "country": "中国",
+      "province": "陕西省",
+      "city": "西安市",
+      "district": "雁塔区",
+      "township": "电子城街道"
+    },
+    "formattedAddress": "陕西省西安市雁塔区电子城街道太白南路2号西安电子科技大学(北校区)",
+    "roads": [],
+    "crosses": [],
+    "pois": []
+  },
+  sfcyglq: 0,
+  sfyzz: 0,
+  qtqk: '',
+  ymtys: 0,
+};
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    cookies: "",
+  },
+
+  onLoad: function () {},
+
+
+  onGetUserInfo: function (e) {
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  onGetOpenid: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  testdaka() {
+    console.log('fdaf');
+    const that = this;
+    wx.request({
+      url: 'https://xxcapp.xidian.edu.cn/uc/wap/login/check',
+      method: 'POST',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      data: {
+        username: '',
+        password: ''
+      },
+      enableCache: true,
+      success(res) {
+        console.log(res);
+        // that.setData({
+        //   cookies: res.header['Set-Cookie']
+        // })
+        // console.log(that.data.cookies);
+        wx.request({
+          url: 'https://xxcapp.xidian.edu.cn/xisuncov/wap/open-report/save',
+          method: 'POST',
+          header: {
+            'cookie': res.header['Set-Cookie'],
+            'content-type': 'application/x-www-form-urlencoded'
+          },
+          data: that.data.submitData,
+          success(res) {
+            console.log(res);
+          }
+        })
+      }
+    })
   }
 })
--- miniprogram/pages/yqt/index.js
+++ miniprogram/pages/yqt/index.js
@@ -1,96 +1,66 @@
-//index.js
-const app = getApp()
-
+// pages/cwj/index.js
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    cookies: "",
-    submitData: {
-      sfzx: 1,
-      tw: 1,
-      area: "陕西省 西安市 雁塔区",
-      city: "西安市",
-      province: "陕西省",
-      address: "陕西省西安市雁塔区电子城街道西安电子科技大学(北校区)",
-      geo_api_info: {
-        "type": "complete",
-        "info": "SUCCESS",
-        "status": 1,
-        "$Da": "jsonp_503270_",
-        "position": {
-          "Q": 34.2325,
-          "R": 108.91436399999999,
-          "lng": 108.9144,
-          "lat": 34.2325
-        },
-        "message": "Get ipLocation success.Get address success.",
-        "location_type": "ip",
-        "accuracy": null,
-        "isConverted": true,
-        "addressComponent": {
-          "citycode": "029",
-          "adcode": "610113",
-          "businessAreas": [{
-            "name": "电子城",
-            "id": "610113",
-            "location": {
-              "Q": 34.205716,
-              "R": 108.91483199999999,
-              "lng": 108.914832,
-              "lat": 34.205716
-            }
-          }],
-          "neighborhoodType": "",
-          "neighborhood": "",
-          "building": "",
-          "buildingType": "",
-          "street": "太白南路",
-          "streetNumber": "2号",
-          "country": "中国",
-          "province": "陕西省",
-          "city": "西安市",
-          "district": "雁塔区",
-          "township": "电子城街道"
-        },
-        "formattedAddress": "陕西省西安市雁塔区电子城街道太白南路2号西安电子科技大学(北校区)",
-        "roads": [],
-        "crosses": [],
-        "pois": []
-      },
-      sfcyglq: 0,
-      sfyzz: 0,
-      qtqk: '',
-      ymtys: 0,
-    }
-  },
-
-  onLoad: function () {},
-
-
-  onGetUserInfo: function (e) {
 
   },
 
-  onGetOpenid: function () {
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
 
   },
 
-  // 上传图片
-  doUpload: function () {
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
 
   },
 
-  handleAccountInput(e) {
-    console.log(e.detail.value);
-    this.setData({
-      inputAccount: e.detail.value
-    });
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
   },
 
-  handlePasswordInput(e) {
-    console.log(e.detail.value);
-    this.setData({
-      inputPassword: e.detail.value
-    });
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
-
 })
